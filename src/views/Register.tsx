084053979158d4d69817254bd66997c459f1df97
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-03-12 23:17:02 +0530

    initial signup page

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,63 +1,137 @@
-import { AxiosResponse } from "axios";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useAuth, registerForm } from "../hooks/Auth";
-import { EMAIL_VALIDATOR } from "../constants/constants";
+import { AxiosResponse } from 'axios';
+import React, { useState } from 'react';
+import { useAuth, registerForm } from '../hooks/Auth';
+import StepOne from '../component/StepOne';
+import { stat } from 'node:fs';
+import StepTwo from '../component/StepTwo';
+import StepThree from '../component/StepThree';
 
 function Register() {
   let auth = useAuth();
   const [isRegistrered, setIsRegistered] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data: registerForm) => {
-    setIsLoading(true);
-    // console.log("Submitted Form Data: ", data);
+  const [state, setState] = useState({
+    name: 'Name',
+    username: 'Username',
+    phone: '+91 XXXX XX XXXX',
+    type: '',
+    nameSHG: 'Name of SHG',
+    productionCap: 'Production Capacity',
+    orderSize: 'Order Size',
+    contact: '+91 XXXX XX XXXX',
+    industryType: '',
+    memberName: 'Name',
+    memberAadhar: 'Aadhar',
+    memberContact: '+91 XXXX XX XXXX',
+    skill: '',
+  });
 
-    auth?.register(data, (response: AxiosResponse) => {
-      // console.log("registration succex");
-      setIsLoading(false);
+  const [step, setStep] = useState(1);
 
-      if (response === undefined || response.status === 500)
-        setMessage("Server is down, please try again later");
-      else if (response.status === 200) setIsRegistered(true);
-      else setMessage(response.data.message);
-    });
+  // const onSubmit = (data: registerForm) => {
+  //   setIsLoading(true);
+  //   // console.log("Submitted Form Data: ", data);
+
+  //   auth?.register(data, (response: AxiosResponse) => {
+  //     // console.log("registration succex");
+  //     setIsLoading(false);
+
+  //     if (response === undefined || response.status === 500)
+  //       setMessage('Server is down, please try again later');
+  //     else if (response.status === 200) setIsRegistered(true);
+  //     else setMessage(response.data.message);
+  //   });
+  // };
+
+  const handleChange = (e: any) => {
+    const { Name, Username, Phone, Type, NameSHG, ProdcutionCap } = e.target;
+
+    setState((prev) => ({
+      ...prev,
+      name: Name,
+      username: Username,
+      phone: Phone,
+      type: Type,
+      nameSHG: NameSHG,
+      productionCap: ProdcutionCap,
+    }));
+  };
+
+  const handleNext = () => {
+    setStep(step + 1);
+  };
+
+  const handleBack = () => {
+    setStep(step - 1);
   };
 
   return (
-    <div className="authform register">
-      <div className="form">
-        {isRegistrered ? (
-          <h1> Verfication link sent to email </h1>
-        ) : (
-          <>
-            <h1>Register</h1>
-            <div className="error">{message}</div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <input
-                name="email"
-                placeholder="Email"
-                ref={register({
-                  required: true,
-                  pattern: EMAIL_VALIDATOR,
-                })}
-              />
-              {errors.email?.type === "required" && (
-                <p>This field is required</p>
-              )}
-              {errors.email?.type === "pattern" && <p>IITKGP email-id only</p>}
+    <div className='authform register'>
+      <div className='form'>
+        <h1>Signup</h1>
+        <div className='error'>{message}</div>
+        <form>
+          <StepOne
+            name={state.name}
+            username={state.username}
+            phone={state.phone}
+            type={state.type}
+            currentStep={step}
+            handleChange={handleChange}
+          />
 
-              <input type="submit" value="Signup" disabled={isLoading} />
-            </form>
-          </>
-        )}
+          {state.type === 'SHG' ? (
+            <>
+              <StepTwo
+                nameSHG={state.nameSHG}
+                productionCap={state.productionCap}
+                orderSize={state.orderSize}
+                contact={state.contact}
+                industryType={state.industryType}
+                type={state.type}
+                currentStep={step}
+                handleChange={handleChange}
+              />
+
+              <StepThree
+                memberName={state.memberName}
+                memberAadhar={state.memberAadhar}
+                memberContact={state.memberContact}
+                skill={state.skill}
+                type={state.type}
+                currentStep={step}
+                handleChange={handleChange}
+              />
+            </>
+          ) : (
+            ''
+          )}
+
+          <input type='button' value='Next' onClick={handleNext} />
+          {step > 1 ? (
+            <input
+              type='button'
+              className='back'
+              value='Back'
+              onClick={handleBack}
+            />
+          ) : (
+            ''
+          )}
+
+          <span>{step}/3</span>
+
+          <hr />
+          <span>If you have an account, please login</span>
+          <a className='button' href='/login' rel='noreferrer noopener'>
+            Login
+          </a>
+        </form>
       </div>
     </div>
   );
 }
-
-// Register.propTypes = {};
 
 export default Register;
