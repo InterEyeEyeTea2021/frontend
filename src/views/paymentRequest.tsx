30031bbc33c299ce56e1d366ce8a9ac6db2c7350
Parth <parthparadkar3@gmail.com>
2021-03-13 21:27:56 +0530

    Add request payments route, shg payments

--- src/views/paymentRequest.tsx
+++ src/views/paymentRequest.tsx
@@ -1,9 +1,18 @@
-import { useAuth, loginForm } from "../hooks/Auth";
+import { paymentForm } from "../hooks/RequestPayment";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../hooks/Auth";
 
-function Payments() {
+function RequestPayment() {
     const [isLoading, setIsLoading] = useState(false);
     let auth = useAuth();
+    const { handleSubmit } = useForm();
+    const currSME = {
+        name: "SME NAME",
+        contact: "XXXX XX XXXX"
+    }
+
+    const currOrder = "Order Name"
 
     const paymentsList = [
         {
@@ -22,6 +31,9 @@ function Payments() {
             status: "paid",
         },
     ]
+    const onSubmit = (data: paymentForm) => {
+        console.log(data)
+    }
 
     return (
         <div className="payments">
@@ -31,20 +43,28 @@ function Payments() {
                 <h1>Bell</h1>
             </div>
             <div>
-                <h2>Machinery</h2>
-                <h1>Pending</h1>
+                <h2>Order</h2>
+                <h1>{currOrder}</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <h2>Reason</h2>
+                    <input
+                        name="reason"
+                        placeholder="Reason"
+                    />
+                    <h2>Description</h2>
+                    <input
+                        name="description"
+                        placeholder="Description"
+                    />
+                </form>
+
             </div>
-            <h1>₹2000</h1>
-            <div>
-                <h1>Order</h1>
-                <h1>Order Name</h1>
-            </div>
-            <h1>SHG</h1>
+            <h1>SME</h1>
             <div>
                 <h1>Box</h1>
                 <div>
-                    <h1>SHG NAME</h1>
-                    <h2>XXXX XX XXXX</h2>
+                    <h1>{currSME.name}</h1>
+                    <h2>{currSME.contact}</h2>
                 </div>
                 <h1>Call icon</h1>
             </div>
@@ -69,4 +89,4 @@ function Payments() {
     )
 }
 
-export default Payments
+export default RequestPayment
