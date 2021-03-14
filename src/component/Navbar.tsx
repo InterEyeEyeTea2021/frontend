0c6acbd82474e5ecd8bfe130158a591539ecc8b3
xypnox <xypnox@gmail.com>
2021-03-15 00:03:56 +0530

    Add logo and setu in header

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -2,49 +2,56 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../hooks/Auth";
 
+import logo from "../images/logo.svg";
+
 export default function Navbar() {
   let auth = useAuth();
 
   return (
     <nav>
       <div className="wrapper">
-        <h1>Drishtee</h1>
-        <ul>
-          <li>
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-          </li>
-          {auth?.user ? (
-            <>
-              <li>
-                <NavLink exact to="/dashboard">
-                  Dashboard
-                </NavLink>
-              </li>
-              <li
-                onClick={() =>
-                  auth?.signout(() => console.log("User signed out"))
-                }
-              >
-                Signout
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <NavLink exact to="/login">
-                  Login
-                </NavLink>
-              </li>
-              <li>
-                <NavLink exact to="/register">
-                  Register
-                </NavLink>
-              </li>
-            </>
-          )}
-        </ul>
+        <header>
+          <img src={logo} alt="Logo" />
+          <h1>GrameenSetu</h1>
+        </header>
+        {auth?.user == null && (
+          <ul>
+            <li>
+              <NavLink exact to="/">
+                Home
+              </NavLink>
+            </li>
+            {auth?.user ? (
+              <>
+                <li>
+                  <NavLink exact to="/dashboard">
+                    Dashboard
+                  </NavLink>
+                </li>
+                <li
+                  onClick={() =>
+                    auth?.signout(() => console.log("User signed out"))
+                  }
+                >
+                  Signout
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink exact to="/login">
+                    Login
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink exact to="/register">
+                    Register
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        )}
       </div>
     </nav>
   );
