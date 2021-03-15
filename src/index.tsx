f22c7680815f1282e0c245483fb124e03258547f
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-03-15 22:55:38 +0530

    provide auth fix;

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ProvideAuth } from "./hooks/Auth";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProvideAuth>
+      <App />
+    </ProvideAuth>
   </React.StrictMode>,
   document.getElementById("root")
 );
