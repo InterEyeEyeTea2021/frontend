b05be5e9b927c70159177947aeea5e4e4b39a645
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-03-16 03:19:39 +0530

    signup for different user types; added password field;

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,6 +1,6 @@
 import { AxiosResponse } from "axios";
 import React, { useState } from "react";
-import { useAuth, registerForm } from "../hooks/Auth";
+import { useAuth, registerForm_SHG, registerForm_SME } from "../hooks/Auth";
 import StepOne from "../component/signupForms/StepOne";
 import { stat } from "node:fs";
 import { useForm } from "react-hook-form";
@@ -20,9 +20,10 @@ function Register() {
     // non user specific
     name: "Name",
     username: "Username",
+    password: "Password",
     phone: "+91 XXXX XX XXXX",
     contact: "+91 XXXX XX XXXX", // for WhatsApp
-    type: "SHG",
+    user_type: "SHG",
     industry_type: "Agriculture, Something, Engineering",
     account_number: "Account Number",
     branch_code: "IFSC Code",
@@ -44,25 +45,54 @@ function Register() {
 
   const [step, setStep] = useState(1);
 
-  // const onSubmit = (data: registerForm) => {
-  //   setIsLoading(true);
-  //   // console.log("Submitted Form Data: ", data);
+  const onSubmit = () => {
+    setIsLoading(true);
 
-  //   auth?.register(data, (response: AxiosResponse) => {
-  //     // console.log("registration succex");
-  //     setIsLoading(false);
+    if (state.user_type === "SHG") {
+      const { address, product_sold, ...data } = state;
+      console.log(data);
 
-  //     if (response === undefined || response.status === 500)
-  //       setMessage('Server is down, please try again later');
-  //     else if (response.status === 200) setIsRegistered(true);
-  //     else setMessage(response.data.message);
-  //   });
-  // };
+      auth?.registerSHG(data, (response: AxiosResponse) => {
+        // console.log("registration succex");
+        setIsLoading(false);
+
+        if (response === undefined || response.status === 500)
+          setMessage("Server is down, please try again later");
+        else if (response.status === 200) setIsRegistered(true);
+        else setMessage(response.data.message);
+      });
+    } else {
+      const {
+        name_SHG,
+        production_cap,
+        order_size,
+        members,
+        member_name,
+        member_aadhar,
+        member_contact,
+        skill,
+        ...data
+      } = state;
+
+      console.log(data);
+
+      auth?.registerSME(data, (response: AxiosResponse) => {
+        // console.log("registration succex");
+        setIsLoading(false);
+
+        if (response === undefined || response.status === 500)
+          setMessage("Server is down, please try again later");
+        else if (response.status === 200) setIsRegistered(true);
+        else setMessage(response.data.message);
+      });
+    }
+  };
 
   const handleNextSubmit = (
     data: Partial<{
       name: string;
       username: string;
+      password: string;
       phone: string;
       contact: string;
       type: string;
@@ -92,7 +122,7 @@ function Register() {
   const handleTypeChange = (type: string) => {
     setState({
       ...state,
-      type,
+      user_type: type,
     });
   };
 
@@ -109,20 +139,21 @@ function Register() {
       <div className="form">
         <div className="title">
           <h1>Signup</h1>
-          {step > 1 ? <p>{state.type}</p> : null}
+          {step > 1 ? <p>{state.user_type}</p> : null}
         </div>
         <div className="error">{message}</div>
         <StepOne
           name={state.name}
           username={state.username}
+          password={state.password}
           phone={state.phone}
-          type={state.type}
+          type={state.user_type}
           currentStep={step}
           handleNextSubmit={handleNextSubmit}
           handleTypeChange={handleTypeChange}
         />
 
-        {state.type === "SHG" ? (
+        {state.user_type === "SHG" ? (
           <>
             <StepTwo
               nameSHG={state.name_SHG}
@@ -130,7 +161,7 @@ function Register() {
               orderSize={state.order_size}
               contact={state.contact}
               industryType={state.industry_type}
-              type={state.type}
+              type={state.user_type}
               currentStep={step}
               handleNextSubmit={handleNextSubmit}
             />
@@ -141,7 +172,7 @@ function Register() {
               memberAadhar={state.member_aadhar}
               memberContact={state.member_contact}
               skill={state.skill}
-              type={state.type}
+              type={state.user_type}
               currentStep={step}
               handleNextSubmit={handleNextSubmit}
             />
@@ -149,7 +180,7 @@ function Register() {
             <Step3
               accountNumber={state.account_number}
               branchCode={state.branch_code}
-              type={state.type}
+              type={state.user_type}
               currentStep={step}
               handleNextSubmit={handleNextSubmit}
             />
@@ -158,14 +189,14 @@ function Register() {
           ""
         )}
 
-        {state.type === "SME" ? (
+        {state.user_type === "SME" ? (
           <>
             <Step2
               address={state.address}
               productSold={state.product_sold}
               contact={state.contact}
               industryType={state.industry_type}
-              type={state.type}
+              type={state.user_type}
               currentStep={step}
               handleNextSubmit={handleNextSubmit}
             />
@@ -173,7 +204,7 @@ function Register() {
             <Step3
               accountNumber={state.account_number}
               branchCode={state.branch_code}
-              type={state.type}
+              type={state.user_type}
               currentStep={step}
               handleNextSubmit={handleNextSubmit}
             />
@@ -183,8 +214,8 @@ function Register() {
         )}
         <form>
           <div className="buttons">
-            {(state.type === "SME" && step > 1 && step < 4) ||
-            (state.type === "SHG" && step > 1 && step < 5) ? (
+            {(state.user_type === "SME" && step > 1 && step < 4) ||
+            (state.user_type === "SHG" && step > 1 && step < 5) ? (
               <input
                 type="button"
                 className="back"
@@ -195,18 +226,18 @@ function Register() {
               ""
             )}
 
-            {(state.type === "SME" && step === 4) ||
-            (state.type === "SHG" && step === 5) ? (
+            {(state.user_type === "SME" && step === 4) ||
+            (state.user_type === "SHG" && step === 5) ? (
               <>
                 <h3> Verify the OTP sent to your mobile number </h3>
                 <span className="label"> OTP </span>
                 <input
                   name="otp"
                   placeholder="OTP"
-                  ref={register({ required: true })}
+                  ref={register({ required: false })}
                 />
 
-                <input type="submit" value="Verify" onClick={handleNext} />
+                <input type="submit" value="Verify" onClick={onSubmit} />
               </>
             ) : (
               ""
