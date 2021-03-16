8a25412198f9efd4fdad784faf33df8e6c1f0993
xypnox <xypnox@gmail.com>
2021-03-16 15:10:41 +0530

    remove unnce

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,6 +1,2 @@
 export const BACKEND_URL = `http://13.234.102.196`;
-export const IMGUR_headers = {
-  "Content-Type": "application/json",
-  Authorization: "Client-ID b79a0e2e466cae5",
-};
 export const API_IMGBB = "629784fe6b8af8814cae40bcd842db0d";
