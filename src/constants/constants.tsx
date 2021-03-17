4a95b54a5911779714e65ef3f000c3a1c95f5f9e
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-03-17 19:48:21 +0530

    changed BACKEND_URL in contants

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,4 +1,4 @@
-export const BACKEND_URL = `http://13.234.102.196`;
+export const BACKEND_URL = `https://api.grameen-setu.com`;
 export const API_IMGBB = "629784fe6b8af8814cae40bcd842db0d";
 
 export const profile_pics = [
