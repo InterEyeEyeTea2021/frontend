3c0595200ef4b962b643f04b40f8b1358e309cc5
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-03-24 19:46:22 +0530

    added youtube tutorial

--- src/views/OrderStatus.tsx
+++ src/views/OrderStatus.tsx
@@ -402,6 +402,17 @@ export default function OrderStatus() {
         ))}
       </div>
 
+      <div>
+        <h3> Skills Tutorial </h3>
+        <iframe
+          src="https://www.youtube.com/embed/aAxGTnVNJiE"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
       <Modal
         isOpen={confirmCompleteOpen}
         // onAfterOpen={afterOpenModal}
